d95f118f6598cae0c63a21a27f08d1d216a7b603
ndrfelipe <ndrfelipebraga@gmail.com>
2025-02-22 22:55:20 -0300

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+import { Post }  from "./Post"
+
+import './global.css';
+
+export function App() {
+  return (
+    <>
+        <Header />
+    </>
+   
+  )
+}
+
